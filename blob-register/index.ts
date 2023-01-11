ef502812ba8f563885a18fca0213297091bfb96f
Jin Igarashi <igarashi.jin@water-gis.com>
2023-01-11 18:19:36 +0000

    revert http-trigger

--- blob-register/index.ts
+++ blob-register/index.ts
@@ -14,8 +14,7 @@ const httpTrigger: AzureFunction = async function (
 	context: Context,
 	req: HttpRequest
 ): Promise<void> {
-	const validationEventType = 'Microsoft.EventGrid.SubscriptionValidationEvent';
-	const storageBlobCreatedEvent = 'Microsoft.Storage.BlobCreated';
+	const url = req.query.url || (req.body && req.body.url);
 
 	if (
 		!AZURE_STORAGE_ACCOUNT ||
@@ -23,55 +22,39 @@ const httpTrigger: AzureFunction = async function (
 		!DATABASE_CONNECTION ||
 		!TITILER_ENDPOINT
 	) {
-		context.log('Please set environment variables for this function');
+		context.res = {
+			status: 500,
+			body: 'Please set environment variables for this function'
+		};
 	} else {
-		const blobManager = new BlobServiceAccountManager(
-			AZURE_STORAGE_ACCOUNT,
-			AZURE_STORAGE_ACCESS_KEY,
-			TITILER_ENDPOINT
-		);
+		if (!url) {
+			context.res = {
+				status: 400,
+				body: 'Please pass a url on the query string or in the request body'
+			};
+		} else {
+			const blobManager = new BlobServiceAccountManager(
+				AZURE_STORAGE_ACCOUNT,
+				AZURE_STORAGE_ACCESS_KEY,
+				TITILER_ENDPOINT
+			);
 
-		for (const events in req.body) {
-			const body = req.body[events];
-			// Deserialize the event data into the appropriate type based on event type
-			if (body.data && body.eventType == validationEventType) {
-				context.log(
-					'Got SubscriptionValidation event data, validation code: ' +
-						body.data.validationCode +
-						' topic: ' +
-						body.topic
-				);
-
-				const code = body.data.validationCode;
-				context.res = { status: 200, body: { ValidationResponse: code } };
-			} else if (body.data && body.eventType == storageBlobCreatedEvent) {
-				const blobCreatedEventData = body.data;
-				context.log(
-					'Relaying received blob created event payload:' + JSON.stringify(blobCreatedEventData)
-				);
-
-				const url = blobCreatedEventData.url;
-
-				// scan file to register
-				const res = await blobManager.scanBlob(url);
-				if (!res.dataset) {
-					context.log('No dataset to register');
-					continue;
-				}
-				const storages: Storages = new Storages([res.storage]);
-				if (storages.getStorages().length === 0) {
-					context.log('The dataset is in invalid storage. Skip to register.');
-					continue;
-				}
-				const datasets = new Datasets([res.dataset]);
-				const dbManager = new DatabaseManager(DATABASE_CONNECTION);
-				await dbManager.register(storages, datasets);
-
-				context.log(`${url} was registered to GeoHub`);
+			// scan file to register
+			const res = await blobManager.scanBlob(url);
+			if (!res.dataset) {
+				throw new Error('No dataset to register');
 			}
+			const storages: Storages = new Storages([res.storage]);
+			const datasets = new Datasets([res.dataset]);
+			const dbManager = new DatabaseManager(DATABASE_CONNECTION);
+			await dbManager.register(storages, datasets);
+
+			context.res = {
+				// status: 200, /* Defaults to 200 */
+				body: `${url} was registered to GeoHub`
+			};
 		}
 	}
-	context.done();
 };
 
 export default httpTrigger;
